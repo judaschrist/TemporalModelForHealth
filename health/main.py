1d582f28a9f9cbc78cc2c2bec62666c36efd83a3
judaschrist <operaphantom.zhang@gmail.com>
2015-08-10 23:37:05 +0800

    fix mulitcore bug on windows

--- health/main.py
+++ health/main.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Lingxiao'
 
-from data.diagnosis import read_diagnosis_from_file
+if __name__ == '__main__':
 
-pdict = read_diagnosis_from_file('F:\\PUPH-data\\diagnosis.csv')
-print(len(pdict))
-print(sum([len(pdict[key]) for key in pdict]))
+    from data.diagnosis import read_diagnosis_from_file
 
-codes = [[dia.name for dia in pdict[key]] for key in pdict]
-# print(codes[0][1].decode('utf-8'))
+    pdict = read_diagnosis_from_file('C:\\Users\\Lingxiao\\Desktop\\diagnosis.csv')
+    print(len(pdict))
+    print(sum([len(pdict[key]) for key in pdict]))
 
-from gensim import corpora
+    codes = [[dia.name for dia in pdict[key]] for key in pdict]
+    # print(codes[0][1].decode('utf-8'))
 
-codedict = corpora.Dictionary(codes)
+    from gensim import corpora
 
-corpus = [codedict.doc2bow(codesperson) for codesperson in codes]
+    codedict = corpora.Dictionary(codes)
+
+    corpus = [codedict.doc2bow(codesperson) for codesperson in codes]
 
 
-from gensim.models.ldamodel import LdaModel
-from gensim.models.ldamulticore import LdaMulticore
-print("start inferring...single")
-import time
-start_time = time.time()
-ldaSingle = LdaModel(corpus, 20)
-print("--- %s seconds ---" % (time.time() - start_time))
-print("start inferring...multi")
-start_time = time.time()
-ldamulti = LdaMulticore(corpus, 20)
-print("--- %s seconds ---" % (time.time() - start_time))
-topics = ldaSingle.show_topics(formatted=False, num_topics=20)
+    from gensim.models.ldamodel import LdaModel
+    from gensim.models.ldamulticore import LdaMulticore
+    print("start inferring...single")
+    import time
+    start_time = time.time()
+    ldaSingle = LdaModel(corpus, 20)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("start inferring...multi")
+    start_time = time.time()
+    ldamulti = LdaMulticore(corpus, 20)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    topics = ldamulti.show_topics(formatted=False, num_topics=20)
 
-count = 0
-for wordlist in topics:
-    count += 1
-    print('topic ' + str(count))
-    for wordturple in wordlist:
-        print('\t' + codedict.__getitem__(int(wordturple[1])) + str(wordturple[0]))
+    count = 0
+    for wordlist in topics:
+        count += 1
+        print('topic ' + str(count))
+        for wordturple in wordlist:
+            print('\t' + codedict.__getitem__(int(wordturple[1])) + str(wordturple[0]))
 
 
