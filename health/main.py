66535f1208ce22ca5aa8a8e2584e95dd826274c0
judaschrist <operaphantom.zhang@gmail.com>
2015-08-10 17:31:51 +0800

    multicore test

--- health/main.py
+++ health/main.py
@@ -3,7 +3,7 @@ __author__ = 'Lingxiao'
 
 from data.diagnosis import read_diagnosis_from_file
 
-pdict = read_diagnosis_from_file('c:\\Users\\Lingxiao\\Desktop\\diagnosis.csv')
+pdict = read_diagnosis_from_file('F:\\PUPH-data\\diagnosis.csv')
 print(len(pdict))
 print(sum([len(pdict[key]) for key in pdict]))
 
@@ -15,10 +15,19 @@ codedict = corpora.Dictionary(codes)
 
 corpus = [codedict.doc2bow(codesperson) for codesperson in codes]
 
-print("start inferring...20")
+
 from gensim.models.ldamodel import LdaModel
-lda = LdaModel(corpus, num_topics=20)
-topics = lda.show_topics(formatted=False, num_topics=20)
+from gensim.models.ldamulticore import LdaMulticore
+print("start inferring...single")
+import time
+start_time = time.time()
+ldaSingle = LdaModel(corpus, 20)
+print("--- %s seconds ---" % (time.time() - start_time))
+print("start inferring...multi")
+start_time = time.time()
+ldamulti = LdaMulticore(corpus, 20)
+print("--- %s seconds ---" % (time.time() - start_time))
+topics = ldaSingle.show_topics(formatted=False, num_topics=20)
 
 count = 0
 for wordlist in topics:
